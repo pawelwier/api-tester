e6dcd7250eaf9ba7524c3c2327a83ce3373d0366
pawel <pawiercinski@gmail.com>
2024-08-14 23:07:46 +0200

    display readable json res

--- src/ui/json_res.rs
+++ src/ui/json_res.rs
@@ -1,18 +1,17 @@
 use eframe::egui::{
     Label, Ui
 };
+use serde_json::to_string_pretty;
 
 pub fn get_json_res_text(ui: &mut Ui, json: &Option<serde_json::Value>) {
-    let mut json_text: String = "".to_string();
-
-    // TODO: parse to be readable
     if let Some(json_option) = json {
         if let Some(json_body) = json_option.as_object() {
-            for (key, value) in json_body {
-                json_text += &format!("{:?}: {:?}\n", key, value);
+            let text: String;
+            match to_string_pretty(json_body) {
+                Ok(json_value) => { text = json_value },
+                Err(_) => { text = "Result is not a valid JSON.".to_owned() }
             }
+            ui.add(Label::new(text));
         }
-        
-        ui.add(Label::new(json_text));
     }
 }
