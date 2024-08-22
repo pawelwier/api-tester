6e04a3d436aac0df80b6ea84de429c1b58aa76ef
pawel <pawiercinski@gmail.com>
2024-08-22 22:49:59 +0200

    display headers json fix

--- src/ui/headers.rs
+++ src/ui/headers.rs
@@ -2,15 +2,32 @@ use eframe::egui::{
     Label, Ui
 };
 use reqwest::header::HeaderMap;
+use serde_json::{
+    Map, from_str, to_string_pretty, to_value, Value
+};
 
 pub fn get_headers_text(ui: &mut Ui, header_map: &Option<HeaderMap>) {
-    let mut headers_text: String = "".to_string();
+    let mut result_json = Map::new();
 
     if let Some(headers) = header_map {
         for (key, value) in headers.iter() {
-            headers_text += &format!("{:?}: {:?}\n", key, value);
+            if let Ok(val) = value.to_str() {
+                let json_result = from_str::<Value>(val);
+
+                let json_value = match json_result {
+                    Ok(data) => data,
+                    Err(_) => to_value(val).unwrap()
+                };
+
+                result_json.insert(key.to_string(), json_value);
+            }
         }
+
+        let text = match to_string_pretty(&result_json) {
+            Ok(data) => data,
+            Err(_) => "Result is not a valid JSON".to_owned()
+        };
         
-        ui.add(Label::new(headers_text));
+        ui.add(Label::new(text));
     }
 }
--- src/ui/json_res.rs
+++ src/ui/json_res.rs
@@ -1,16 +1,17 @@
 use eframe::egui::{
     Label, Ui
 };
-use serde_json::to_string_pretty;
+use serde_json::{
+    to_string_pretty, Value
+};
 
-pub fn get_json_res_text(ui: &mut Ui, json: &Option<serde_json::Value>) {
+pub fn get_json_res_text(ui: &mut Ui, json: &Option<Value>) {
     if let Some(json_option) = json {
         if let Some(json_body) = json_option.as_object() {
-            let text: String;
-            match to_string_pretty(json_body) {
-                Ok(json_value) => { text = json_value },
-                Err(_) => { text = "Result is not a valid JSON.".to_owned() }
-            }
+            let text: String = match to_string_pretty(json_body) {
+                Ok(data) => data,
+                Err(_) => "Result is not a valid JSON.".to_owned()
+            };
             ui.add(Label::new(text));
         }
     }
